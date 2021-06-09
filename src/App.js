c7c858cdec62b2d504382d2937d972a408cc39e9
Quang Tran <trannhatquang3112@gmail.com>
2021-06-10 01:05:40 +0800

    Add ant design & restyle form

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import ClusterVis from './components/ClusterVis'
 import PseudoCodeBox from './components/PseudoCodeBox'
 import DataDisplay from './components/DataDisplay'
 import MainComponent from './MainComponent'
-import { Button, TextField } from '@material-ui/core'
+import { TextField } from '@material-ui/core'
 import { pseudo_code, KMeans } from './ml/k-means'
 import { range } from 'd3';
+import { Form, Input, Button } from 'antd';
 
 var points = [
   [120, 20],
@@ -19,21 +20,20 @@ var points = [
   [110, 190]
 ];
 var k = 2;
+var n_input, k_input;
 
 function App() {
-  
+
   var [state_arr, setStateArr] = React.useState(0);
-  
+
   console.log("COMPUTE");
   console.log(k);
   state_arr = KMeans(points, k);
-  
+
   const clusterVisRef = React.useRef();
   const [refVisible, setRefVisible] = React.useState(false);
 
-  var n_input, k_input;
-
-  function generateRandomPoints(evt) {
+  function generateRandomPoints() {
     console.log("DATA HERE");
     console.log(n_input);
     points = [];
@@ -46,34 +46,39 @@ function App() {
     points = points;
     k = k_input;
     setStateArr(KMeans(points, k_input));
-    evt.preventDefault();
-  }
-
-  function updateK(evt) {
-    k_input = parseInt(evt.target.value);
-  }
-
-  function updateN(evt) {
-    n_input = parseInt(evt.target.value);
   }
 
   React.useEffect(() => {
     console.log(clusterVisRef.current);
   }, [clusterVisRef]);
 
+  const onFormChange = ({ numPoints, numCentroids }) => {
+    if (numPoints) {
+      n_input = parseInt(numPoints)
+    }
+    if (numCentroids) {
+      k_input = parseInt(numCentroids)
+    }
+  };
+
   return (
     <div>
-      <form onSubmit={generateRandomPoints}>
-        <label>
-          Number of Points:
-          <TextField onChange={updateN} />
-        </label>
-        <label>
-          Number of Centroids:
-          <TextField onChange={updateK} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <Form
+        layout="inline"
+        onFinish={generateRandomPoints}
+        onValuesChange={onFormChange}
+      >
+        <Form.Item name="numPoints" label="Number of points">
+          <Input placeholder="Input a number" />
+        </Form.Item>
+        <Form.Item name="numCentroids" label="Number of centroids">
+          <Input placeholder="Input a number" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" onClick={generateRandomPoints}>Submit</Button>
+        </Form.Item>
+      </Form>
+
       <MainComponent id='main_component' state_arr={state_arr} />
     </div>
   );
